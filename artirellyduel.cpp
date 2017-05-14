9b5abd768c461ef06ff315786768d1b2dd3d61a7
adrianlaszka <adrian.laszka90@gmail.com>
2017-05-14 21:31:04 +0200

    state driven

--- artirellyduel.cpp
+++ artirellyduel.cpp
@@ -2,15 +2,20 @@
 #include "artirellyduel.hpp"
 
 #include "resources.hpp"
+#include <stdlib.h>
 
 ArtirellyDuel::ArtirellyDuel() :
     Widget(NULL, 0, 0, 0, 640, 480),
     _ground(this, __IDC__GROUND, 0, 0),
+    _player1(this, __IDC__PLAYER1),
+    _player2(this, __IDC__PLAYER2),
     _toolbar(this, __IDC__TOOLBAR, 0, 400) {
     gout.open(640, 480);
-
     _ground.add(_widgets);
     _toolbar.add(_widgets);
+    _player1.add(_widgets);
+    _player2.add(_widgets);
+    initialize();
 }
 
 void ArtirellyDuel::run() {
@@ -29,7 +34,7 @@ void ArtirellyDuel::run() {
         }
         if (focus != -1) {
             _widgets[focus]->setFocus(true);
-            _widgets[focus]->handle(ev);
+            _widgets[focus]->handle(NULL, 0, ev);
         }
 
         // draw
@@ -41,21 +46,74 @@ void ArtirellyDuel::run() {
     }
 }
 
-void ArtirellyDuel::handle(event ev) {
-    if (ev.type == ev_widget) {
-        Widget * widget = (Widget *)ev.widget;
 
+void ArtirellyDuel::initialize() {
+  int positionX = 0;
+  int positionY = 0;
+
+  // initialize area
+  _ground.initialize();
+
+  // generate player1 position
+  positionX = (rand() % 100) + _player1.getSizeX();
+  positionY = _ground.smooth(positionX, _player1.getSizeX() / 2);
+  _player1.setPositionX(positionX - (_player1.getSizeX() / 2));
+  _player1.setPositionY(_ground.getSizeY() - (positionY + _player1.getSizeY()));
+  _player1.setAngle(20);
+
+  // generate player2 position
+  positionX = _ground.getSizeX() - ((rand() % 100) + _player2.getSizeX());
+  positionY = _ground.smooth(positionX, _player2.getSizeX() / 2);
+  _player2.setPositionX(positionX - (_player2.getSizeX() / 2));
+  _player2.setPositionY(_ground.getSizeY() - (positionY + _player2.getSizeY()));
+  _player2.setAngle(160);
+
+  // set initial state
+  if ((rand() % 2) == 0) {
+    _state = PLAYER1_SETUP;
+  } else {
+    _state = PLAYER2_SETUP;
+  }
+}
+
+
+void ArtirellyDuel::handle(Widget * widget, int code, event ev) {
+    if (widget != NULL) {
         switch (widget->getId()) {
         case __IDC__TOOLBAR__BUTTON__NEW_GAME : {
-            switch (ev.code) {
+            switch (code) {
             case __MESSAGE__OnClick : {
-              _ground.initialize();
+              initialize();
               break;
             }
             }
             break;
         }
         }
-
     }
+
+printf("STATE : %d\n", _state);
+
+    switch (_state) {
+    case PLAYER1_SETUP : {
+        break;
+    }
+    case PLAYER1_SHOT: {
+        break;
+    }
+    case PLAYER2_SHOT: {
+        break;
+    }
+    case PLAYER2_SETUP : {
+        break;
+    }
+    case GAME_OVER : {
+        break;
+    }
+    }
+/*
+
+
+
+    */
 }
--- artirellyduel.hpp
+++ artirellyduel.hpp
@@ -5,6 +5,7 @@
 
 #include "widget.hpp"
 #include "ground.hpp"
+#include "artirelly.hpp"
 #include "toolbar.hpp"
 
 using namespace std;
@@ -14,16 +15,30 @@ protected:
     vector<Widget *> _widgets;
 
     Ground _ground;
+    Artirelly _player1;
+    Artirelly _player2;
     Toolbar _toolbar;
+
+    typedef enum {
+      PLAYER1_SETUP,
+      PLAYER1_SHOT,
+      PLAYER2_SETUP,
+      PLAYER2_SHOT,
+      GAME_OVER
+    } states;
+
+    states _state;
 
 public:
     ArtirellyDuel();
 
+    // initialize game
+    void initialize();
     // run game
     void run();
 
 //  virtual void draw() const ;
-    virtual void handle(event ev);
+    virtual void handle(Widget * widget, int code, event ev);
 };
 
 #endif // BUTTON_HPP_INCLUDED
